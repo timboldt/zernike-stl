70ae68b88c0d338ac7a7ca6b30c0a180308b04be
Tim Boldt <tim.boldt@gmail.com>
2021-03-26 13:14:06 -0700

    Basic surface rendering

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/timboldt/zernike-stl
 
 go 1.16
 
-require github.com/hschendel/stl v1.0.4 // indirect
+require github.com/hschendel/stl v1.0.4
--- main.go
+++ main.go
@@ -1,47 +1,77 @@
 package main
 
-import "fmt"
-import "github.com/hschendel/stl"
+import (
+	"fmt"
+
+	"github.com/hschendel/stl"
+)
 
 func main() {
 	s := &stl.Solid{
-		Name:    "Simple",
+		Name:    "Zernike Shapes",
 		IsAscii: true,
-		Triangles: []stl.Triangle{
-			{
-				Normal: stl.Vec3{0, 0, -1},
-				Vertices: [3]stl.Vec3{
-					{0, 0, 0},
-					{0, 1, 0},
-					{1, 0, 0},
-				},
-			},
-			{
-				Normal: stl.Vec3{0, -1, 0},
-				Vertices: [3]stl.Vec3{
-					{0, 0, 0},
-					{1, 0, 0},
-					{0, 0, 1},
-				},
-			},
-			{
-				Normal: stl.Vec3{0.57735, 0.57735, 0.57735},
-				Vertices: [3]stl.Vec3{
-					{0, 0, 1},
-					{1, 0, 0},
-					{0, 1, 0},
-				},
-			},
-			{
-				Normal: stl.Vec3{-1, 0, 0},
-				Vertices: [3]stl.Vec3{
-					{0, 0, 0},
-					{0, 0, 1},
-					{0, 1, 0},
-				},
-			},
-		},
 	}
-	s.WriteFile("/tmp/test.stl")
-	fmt.Println("hi")
+	heights := [][]float32{
+		{1, 3, 2},
+		{3, 3, 4},
+		{2, 4, 2},
+	}
+	for row := range heights {
+		if row == 0 {
+			continue
+		}
+		for col := range heights[row] {
+			if col == 0 {
+				continue
+			}
+			s.AppendTriangle(stl.Triangle{
+				Vertices: [3]stl.Vec3{
+					{float32(row - 1), float32(col - 1), heights[row-1][col-1]},
+					{float32(row), float32(col - 1), heights[row][col-1]},
+					{float32(row), float32(col), heights[row][col]},
+				},
+			})
+			s.AppendTriangle(stl.Triangle{
+				Vertices: [3]stl.Vec3{
+					{float32(row - 1), float32(col - 1), heights[row-1][col-1]},
+					{float32(row), float32(col), heights[row][col]},
+					{float32(row - 1), float32(col), heights[row-1][col]},
+				},
+			})
+			fmt.Printf("row %d col %d\n", row, col)
+		}
+	}
+	// 	Triangles: []stl.Triangle{
+	// 		{
+	// 			Vertices: [3]stl.Vec3{
+	// 				{0, 0, 0},
+	// 				{0, 1, 0},
+	// 				{1, 0, 0},
+	// 			},
+	// 		},
+	// 		{
+	// 			Vertices: [3]stl.Vec3{
+	// 				{0, 0, 0},
+	// 				{1, 0, 0},
+	// 				{0, 0, 1},
+	// 			},
+	// 		},
+	// 		{
+	// 			Vertices: [3]stl.Vec3{
+	// 				{0, 0, 1},
+	// 				{1, 0, 0},
+	// 				{0, 1, 0},
+	// 			},
+	// 		},
+	// 		{
+	// 			Vertices: [3]stl.Vec3{
+	// 				{0, 0, 0},
+	// 				{0, 0, 1},
+	// 				{0, 1, 0},
+	// 			},
+	// 		},
+	// 	},
+	// }
+	s.RecalculateNormals()
+	s.WriteFile("/Users/timboldt/Desktop/test.stl")
 }
